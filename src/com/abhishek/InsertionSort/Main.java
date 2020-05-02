cdc9692f71f8d4c7aaa84d0d0f8210e0e0449408
Abhishek Kumar <abhishek.kumar@adesa.com>
2020-05-02 15:05:59 +0530

    first commit

---
+++ src/com/abhishek/InsertionSort/Main.java
@@ -0,0 +1,19 @@
+package com.abhishek.InsertionSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int intArray[] = {20, 35, -15, 7, 55, 1, -22};
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int i;
+            int newElement = intArray[firstUnsortedIndex];
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+            intArray[i] = newElement;
+        }
+        System.out.println(Arrays.toString(intArray));
+    }
+}
